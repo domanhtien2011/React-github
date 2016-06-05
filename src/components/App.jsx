15e306989dfbbf02ad5f28496ce20e3b252fc421
Tien Do <domanhtien2011@gmail.com>
2016-06-05 14:21:50 +0700

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React;
+import ReactDOM from 'react-dom';
+
+class App extends Components{
+  render(){
+    return(
+      <div>
+        My React Experiment
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+  // the second parameter means that where we it to show up
+);
